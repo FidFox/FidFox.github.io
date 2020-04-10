edf3c5fad73781b637c2d0fc395605ceaedf288f
Wilson Daniel Ospina <wdospinal@unal.edu.co>
2020-04-10 16:12:18 -0500

    initial

---
+++ precache-manifest.48f35c31dcf604609c64decc76a55b6e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeaa9300b658cc010bb5",
+    "url": "/static/js/app.6a84cbba.chunk.js"
+  },
+  {
+    "revision": "328ec0abaf4b8f214d7f",
+    "url": "/static/js/runtime~app.b3244af7.js"
+  },
+  {
+    "revision": "690f24aea546000ac2d4",
+    "url": "/static/js/2.fa232ee6.chunk.js"
+  },
+  {
+    "revision": "3388c959ebd8e5f4029bdb4b53ef860d",
+    "url": "/static/media/happy.3388c959.jpg"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "b76de3d0361b0c1da791050544a86aac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb7e30f9549ab72559f7a7bc2140098d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
